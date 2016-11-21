30e3eb8870d85237fd5a3bcfd4718b3e34aa4989
Adam Blanchard <adam.blanchard@skybettingandgaming.com>
2016-11-21 15:38:55 +0000

    Added qubit helper and test

---
+++ src/Helpers/QubitHelper.js
@@ -0,0 +1,27 @@
+class QubitHelper {
+    trackAction(action, callback)
+    {
+        if (!action) {
+            throw new Error("Missing argument: action");
+        }
+
+        if (callback) {
+            callback();
+        }
+    }
+
+    triggerExperiment(experiment, callback)
+    {
+        if (!experiment) {
+            throw new Error("Missing argument: experiment");
+        }
+
+        if (callback) {
+            callback(0);
+        }
+
+        return 0;
+    }
+}
+
+export default new QubitHelper();
---
+++ tests/Helpers/QubitHelper.test.js
@@ -0,0 +1,53 @@
+import QubitHelper from '../../src/Helpers/QubitHelper';
+import sinon from 'sinon';
+import assert from 'assert';
+
+describe('QubitHelper', () => {
+
+    describe('trackAction', () => {
+        it('calls callback', () => {
+            let callback = sinon.spy();
+            QubitHelper.trackAction('action', callback);
+
+            sinon.assert.calledOnce(callback);
+        });
+
+        it('does not break when there is no callback', () => {
+            QubitHelper.trackAction('action');
+        });
+
+        it('throws an error if action is not an argument', () => {
+            assert.throws(QubitHelper.trackAction, /action/);
+        });
+    });
+
+    describe('triggerExperiment', () => {
+        it('calls callback', () => {
+            let callback = sinon.spy();
+            QubitHelper.triggerExperiment('experiment', callback);
+
+            sinon.assert.calledOnce(callback);
+        });
+
+        it('does not break when there is no callback', () => {
+            QubitHelper.triggerExperiment('experiment');
+        });
+
+        it('calls callback with variant id', () => {
+            let callback = sinon.spy();
+            QubitHelper.triggerExperiment('experiment', callback);
+
+            sinon.assert.calledWith(callback, 0);
+        });
+
+        it('should return the variant id', () => {
+            let variant = QubitHelper.triggerExperiment('experiment');
+            assert.equal(0, variant);
+        });
+
+        it('throws an error if experiment is not an argument', () => {
+            assert.throws(QubitHelper.triggerExperiment, /experiment/);
+        });
+    });
+
+});
