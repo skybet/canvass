de5bddfa3cd046ccf7625439eca1b81d5e571316
Adam Blanchard <adam.blanchard@skybettingandgaming.com>
2016-11-24 16:53:04 +0000

    [refs LVS-969] updated doc blocks of experiment with access

--- src/Toolkit/Experiment.js
+++ src/Toolkit/Experiment.js
@@ -6,6 +6,11 @@ class Experiment
         ACTIVE: 'ACTIVE'
     };
 
+    /**
+     * @public
+     * @param {string} id Experiment ID
+     * @param {Trigger[]} triggers Array of experiment triggers
+     */
     constructor(id, triggers, variants) {
         if (!id) {
             throw new Error('Missing argument: id');
@@ -35,6 +40,8 @@ class Experiment
 
     /**
      * Trigger the experiment
+     *
+     * @public
      */
     trigger() {
         this.setStatus(Experiment.Status.TRIGGERED);
@@ -43,6 +50,7 @@ class Experiment
     /**
      * Get the experiment id
      *
+     * @public
      * @returns {string} id
      */
     getId() {
@@ -52,6 +60,7 @@ class Experiment
     /**
      * Get the variant for this instance of the experiment
      *
+     * @public
      * @returns {mixed} variant
      */
     getVariant() {
@@ -64,6 +73,7 @@ class Experiment
     /**
      * Sets the status of the experiment
      *
+     * @public
      * @param {string} status The status to set the experiment to
      */
     setStatus(status) {
@@ -80,6 +90,7 @@ class Experiment
     /**
      * Get the status of the experiment
      *
+     * @public
      * @returns {string} The status of the experiment
      */
     getStatus() {
@@ -89,6 +100,7 @@ class Experiment
     /**
      * Set the group of the experiment
      *
+     * @public
      * @param {string} group The group that the experiment instance belongs to
      */
     setGroup(group) {
@@ -99,6 +111,7 @@ class Experiment
     /**
      * Gets the group of the experiment
      *
+     * @public
      * @returns {string} Group of experiment
      */
     getGroup() {
@@ -108,6 +121,7 @@ class Experiment
     /**
      * Subscribe to listen to changes
      *
+     * @public
      * @param {mixed} observer The listener of the changes
      */
     subscribe(observer) {
@@ -119,6 +133,7 @@ class Experiment
     /**
      * Unsubscribe to stop listening for changes
      *
+     * @public
      * @param {mixed} observer The listener to remove
      */
     unsubscribe(observer) {
@@ -132,6 +147,7 @@ class Experiment
     /**
      * Notify all observers of a change
      *
+     * @public
      * @param {mixed} payload The payload to pass to the observers
      */
     emit(payload) {
@@ -143,6 +159,8 @@ class Experiment
     /**
      * Called when observed objects change. Checks if all triggers have been fired
      * and change status of the experiment if necessary.
+     *
+     * @public
      */
     notify() {
         if (this.status === Experiment.Status.ACTIVE) {
@@ -150,13 +168,14 @@ class Experiment
         }
 
         if (this.haveTriggersFired()) {
-            this.setStatus(Experiment.Status.TRIGGERED);
+            this.trigger();
         }
     }
 
     /**
      * Checks if all the triggers have fired
      *
+     * @private
      * @return {bool} If all the triggered have fired
      */
     haveTriggersFired() {
