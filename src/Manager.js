a9ac2946d9c2c663f3e2127ecbe71ec6f1167589
Stevan Goode <stevan.goode@skybettingandgaming.com>
2016-11-29 10:22:56 +0000

    [refs LVS-988] Added scopes to jsblocks

--- src/Manager.js
+++ src/Manager.js
@@ -3,6 +3,7 @@ import Experiment from './Experiment';
 class Manager
 {
     /**
+     * @public
      * @param {Registry} registry The experiment registry to use
      * @param {Helper} helper The helper to use to communicate with the experiment reporting system
      */
@@ -14,6 +15,7 @@ class Manager
     /**
      * Adds an experiment to the registry and wires it up
      *
+     * @public
      * @param {Experiment} experiment The experiment to be added
      */
     addExperiment(experiment) {
@@ -25,6 +27,7 @@ class Manager
      * Activates an experiment. Contacts the experiment reporting system via the helper and gets
      * a group for this user
      *
+     * @private
      * @param {string} experimentId The unique ID for the experiment being activated
      */
     activateExperiment(experimentId) {
