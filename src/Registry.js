42bfdd270067221fe4a98d482390a7a90831abbb
Stevan Goode <stevan.goode@skybettingandgaming.com>
2016-11-29 09:14:30 +0000

    [refs LSV-988] Removed commented out, old code

--- src/Registry.js
+++ src/Registry.js
@@ -32,32 +32,6 @@ class Registry
         }
         return this.register[id];
     }
-
-    // /**
-    //  * Notify the registry that some experiment has updated and trigger the experiment
-    //  * if necessary.
-    //  *
-    //  * @public
-    //  * @param {string} experimentId ID of the experiment that has changed
-    //  */
-    // notify(experimentId) {
-    //     let experiment = this.getExperiment(experimentId);
-    //     let experimentStatus = experiment.getStatus();
-    //     if (experimentStatus === Experiment.Status.TRIGGERED) {
-    //         this.triggerExperiment(experimentId);
-    //     }
-    // }
-    //
-    // /**
-    //  * Triggers an experiment in the registry
-    //  *
-    //  * @private
-    //  * @param {string} id ID of the experiment to trigger
-    //  */
-    // triggerExperiment(id) {
-    //     let group = this.helper.triggerExperiment(id);
-    //     this.register[id].setGroup(group);
-    // }
 }
 
 export default Registry;
