1e41bea65c2fbcdaf28541a30b95c6346bb031e5
Adam Blanchard <adam.blanchard@skybettingandgaming.com>
2017-02-01 15:17:08 +0000

    [refs SVD-114] added missing config

---
+++ src/Config.js
@@ -0,0 +1,38 @@
+import cookie from 'cookie';
+import Logger from '~/src/Helpers/Logger';
+
+export class Config
+{
+    static defaults = {
+        debug: false,
+        disableActivation: false,
+    };
+
+    constructor(config) {
+        this.config = Object.assign({}, this.defaults, config);
+
+        if (typeof document !== 'undefined') {
+            let cookies = cookie.parse(document.cookie);
+            if (cookies.canvassDisableActivation) {
+                this.set('disableActivation', true);
+                Logger.info('Detected "disableActivation" cookie. Disabling activation of experiments.');
+            }
+
+            if (cookies.canvassDebug) {
+                this.set('debug', true);
+                Logger.info('Detected "debug" cookie. Enabling debug logging.');
+            }
+        }
+    }
+
+    get(key) {
+        return this.config[key];
+    }
+
+    set(key, value) {
+        this.config[key] = value;
+    }
+
+}
+
+export default new Config();
