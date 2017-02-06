866ebe7645f5cab1f789bd1a83d203f5b7345990
Adam Blanchard <adam.blanchard@skybettingandgaming.com>
2017-02-06 11:32:58 +0000

    [refs SVD-114] added doc blocks to logger

--- src/Helpers/Logger.js
+++ src/Helpers/Logger.js
@@ -9,27 +9,62 @@ export class Logger
 {
     static PREFIX = '[canvass]';
 
+    /**
+     * @public
+     * @param {logger} [logger] Logging instance such as console.log
+     * @param {Logger.LEVEL} [outputLevel] Level of logging to output
+     */
     constructor(logger, outputLevel) {
         this.logger = logger || console;
         this.outputLevel = outputLevel || LEVEL.WARN;
     }
 
+    /**
+     * Sets the logging object to use
+     *
+     * @public
+     * @param {logger} logger Logging instance such as console.log
+     */
     setLogger(logger) {
         this.logger = logger;
     }
 
+    /**
+     * Outputs an error
+     *
+     * @public
+     * @param {...*} args Objects to output
+     */
     error() {
         this.logger.error(...this.formatMessages(Array.prototype.slice.call(arguments)));
     }
 
+    /**
+     * Outputs a warning
+     *
+     * @public
+     * @param {...*} args Objects to output
+     */
     warn() {
         this.logger.warn(...this.formatMessages(Array.prototype.slice.call(arguments)));
     }
 
+    /**
+     * Outputs an info message
+     *
+     * @public
+     * @param {...*} args Objects to output
+     */
     info() {
         this.logger.info(...this.formatMessages(Array.prototype.slice.call(arguments)));
     }
 
+    /**
+     * Outputs a debug message
+     *
+     * @public
+     * @param {...*} args Objects to output
+     */
     debug() {
         if (this.outputLevel === LEVEL.DEBUG) {
             let useMethod = this.logger.debug || this.logger.log;
@@ -37,11 +72,23 @@ export class Logger
         }
     }
 
+    /**
+     * Outputs a table
+     *
+     * @public
+     * @param {object} data Data object or array to output
+     */
     table(data) {
         let useMethod = this.logger.table || this.logger.log;
         useMethod(data);
     }
 
+    /**
+     * Formats arguments to the logging functions to include the prefix
+     *
+     * @public
+     * @param {...*} args Objects to output
+     */
     formatMessages(messages) {
         if (messages.length < 1) {
             return messages;
@@ -51,6 +98,12 @@ export class Logger
         return messages;
     }
 
+    /**
+     * Sets the output level
+     *
+     * @public
+     * @param {Logger.LEVEL} [outputLevel] Level of logging to output
+     */
     setOutputLevel(outputLevel) {
         this.outputLevel = outputLevel;
     }
