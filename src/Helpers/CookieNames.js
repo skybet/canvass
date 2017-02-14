8212fc1a0d876d18c5479d1969fa0d1473b7093f
Adam Blanchard <adam.blanchard@skybettingandgaming.com>
2017-02-14 13:18:23 +0000

    [refs SVD-115] ... commiting the new file would be useful

---
+++ src/Helpers/CookieNames.js
@@ -0,0 +1,9 @@
+const CookieNames = {
+    DISABLE_ACTIVATION: 'canvassDisableActivation',
+    DEBUG: 'canvassDebug',
+    TRIGGERED_EXPERIMENTS: 'canvassTriggeredExperiments',
+};
+
+Object.freeze(CookieNames);
+
+export default CookieNames;
