fb771748b30b91ea1595e30f9daf9c12782e0874
Adam Blanchard <adam.blanchard@skybettingandgaming.com>
2017-04-20 09:12:24 +0100

    [POR-17] Added doc blocks for PreviewModeHelper

--- src/Config/PreviewModeHelper.js
+++ src/Config/PreviewModeHelper.js
@@ -9,20 +9,13 @@ export const PreviewModes = {
 
 export default
 {
-    saveToSessionStorage(mode, experiments) {
-        console.log('saving to session storage');
-        if (sessionStorage) {
-            // If turning preview mode off, just remove the key
-            if (mode === PreviewModes.OFF) {
-                sessionStorage.removeItem('canvassPreviewMode');
-                sessionStorage.removeItem('canvassPreviewModeExperiments');
-            } else {
-                sessionStorage.setItem('canvassPreviewMode', mode);
-                sessionStorage.setItem('canvassPreviewModeExperiments', JSON.stringify(experiments));
-            }
-        }
-    },
-
+    /**
+     * Parses the preview mode config from the query string and session storage. Query
+     * string config will always override anything stored in session storage.
+     *
+     * @public
+     * @returns {object} Preview mode and preview experiments
+     */
     parse() {
         const previewModeQueryString = this.parseQueryString();
         const previewModeSessionStorage = this.parseSessionStorage();
@@ -41,6 +34,12 @@ export default
         return previewMode;
     },
 
+    /**
+     * Parses preview mode config from session storage
+     *
+     * @private
+     * @returns {object} Preview mode and preview experiments
+     */
     parseSessionStorage() {
         const sessionStorage = this.getSessionStorage();
         const mode = sessionStorage ? sessionStorage.getItem('canvassPreviewMode') : null;
@@ -49,6 +48,12 @@ export default
         return mode ? {mode, experiments} : null;
     },
 
+    /**
+     * Parses preview mode config from the query string
+     *
+     * @private
+     * @returns {object} Preview mode and preview experiments
+     */
     parseQueryString() {
         if (!window) {
             return null;
@@ -76,14 +81,53 @@ export default
         return null;
     },
 
+    /**
+     * Saves the preview mode and preview experiments to session storage
+     *
+     * @public
+     * @param {string} mode The preview mode
+     * @param {object} experiments The experiment and group pairs
+     */
+    saveToSessionStorage(mode, experiments) {
+        if (sessionStorage) {
+            // If turning preview mode off, just remove the key
+            if (mode === PreviewModes.OFF) {
+                sessionStorage.removeItem('canvassPreviewMode');
+                sessionStorage.removeItem('canvassPreviewModeExperiments');
+            } else {
+                sessionStorage.setItem('canvassPreviewMode', mode);
+                sessionStorage.setItem('canvassPreviewModeExperiments', JSON.stringify(experiments));
+            }
+        }
+    },
+
+    /**
+     * Gets the location search value
+     *
+     * @private
+     * @returns {string} window.location.search
+     */
     getWindowLocationSearch() {
         return window.location.search;
     },
 
+    /**
+     * Gets the sessionStorage object
+     *
+     * @private
+     * @returns {object} sessionStorage
+     */
     getSessionStorage() {
         return sessionStorage;
     },
 
+    /**
+     * Checks if the data passed in is a JSON object
+     *
+     * @private
+     * @param {*} data Data to check
+     * @returns {boolean} Whether or not the data is a JSON object
+     */
     isJson(data) {
         let jsonData;
 
